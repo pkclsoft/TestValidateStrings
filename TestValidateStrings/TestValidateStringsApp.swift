ae379b0b9ba7035ff53d103a7fe0a1715834c30c
Peter Easdown <info@pkclsoft.com>
2022-11-08 22:14:02 +1100

    Initial Commit

---
+++ TestValidateStrings/TestValidateStringsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestValidateStringsApp.swift
+//  TestValidateStrings
+//
+//  Created by Peter Easdown on 8/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestValidateStringsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
